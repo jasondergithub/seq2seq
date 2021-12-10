e6fd73c34646f91864c353ad5fda66c969c40b2e
Jason Liu <j84yes116@gmail.com>
2021-12-11 00:27:16 +0800

    still unfinished

--- main/engine.py
+++ main/engine.py
@@ -8,18 +8,40 @@ from torch.utils.data import DataLoader
 def loss_fn(outputs, targets):
     return nn.BCEWithLogitsLoss()(outputs, targets)
 
-def train_fn(src_loader, trg_loader):
+def train_fn(src_loader, trg_loader, model, optimizer, device, scheduler):
     
+    model.train()
+    fin_targets = []
+    fin_outputs = []
+    running_loss = 0    
+
     for (src_data, trg_data) in tqdm(zip(src_loader, trg_loader)):
         
         src_tokens_tensor = src_data['tokens_tensor']
         src_segments_tensor = src_data['segments_tensor']
         src_masks_tensor = src_data['masks_tensor']
+        
+        src_tokens_tensor = src_tokens_tensor.to(device, dtype=torch.long)
+        src_segments_tensor = src_segments_tensor.to(device, dtype=torch.long)
+        src_masks_tensor = src_masks_tensor.to(device, dtype=torch.long)
+
+        reference = [src_tokens_tensor, src_segments_tensor, src_masks_tensor]
 
         trg_tokens_tensor = trg_data['tokens_tensor']
         trg_segments_tensor = trg_data['segments_tensor']
         trg_masks_tensor = trg_data['masks_tensor']
-        trg_label = trg_data['target']          
+        trg_label = trg_data['target']
+
+        trg_tokens_tensor = trg_tokens_tensor.to(device, dtype=torch.long)
+        trg_segments_tensor =trg_segments_tensor.to(device, dtype=torch.long)
+        trg_masks_tensor = trg_masks_tensor.to(device, dtype=torch.long)   
+        trg_label_tensor = trg_label.to(device, dtype=torch.float)
+
+        target = [trg_tokens_tensor, trg_segments_tensor, trg_masks_tensor, trg_label_tensor]
+
+        optimizer.zero_grad()
+        outputs = model(reference, target)
+
      
 
 
