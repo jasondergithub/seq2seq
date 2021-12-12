784f4b76b36f2e20f1f3c0e2b74b56d063e21787
Jason Liu <j84yes116@gmail.com>
2021-12-12 22:14:35 +0800

    decrease num_layers

--- main/config.py
+++ main/config.py
@@ -10,6 +10,6 @@ MODEL_PATH = "../dict/saved_model.pth"
 bert_config = BertConfig.from_pretrained('bert-base-chinese')
 bert_config.num_labels = 1
 bert_config.max_position_embeddings = 1500
-bert_config.num_hidden_layers = 6
+bert_config.num_hidden_layers = 4
 bert_config.num_attention_heads = 2
 bert_config.output_hidden_states=True
