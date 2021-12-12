8df3cdb354b3acc240807e65c9cfd122497858a1
Jason Liu <j84yes116@gmail.com>
2021-12-12 23:05:38 +0800

    make a new pairing table

--- preprocessing/load_table.py
+++ preprocessing/load_table.py
@@ -10,12 +10,20 @@ with open("../dict/unrelatedTable.txt", "rb") as fp:
 name_list = ['table1.txt', 'table2.txt', 'table3.txt', 'table4.txt', 'table5.txt', 'table6.txt', 
 'table7.txt', 'table8.txt', 'table9.txt', 'table10.txt', 'table11.txt', 'table12.txt', 'table13.txt', 'table14.txt',
 'table15.txt', 'table16.txt', 'table17.txt', 'table18.txt', 'table19.txt', 'table20.txt']
-for i in range(20):
-    if (i+1) % 5 == 0:
-        subtable = random.sample(unrelated_table, 2762)
-    else:
-        subtable = random.sample(unrelated_table, 1381)    
-    subtable = subtable + related_table
-    random.shuffle(subtable)
-    with open("../table/" + name_list[i], "wb") as fp:
-        pickle.dump(subtable, fp)
+# for i in range(20):
+#     if (i+1) % 5 == 0:
+#         subtable = random.sample(unrelated_table, 2762)
+#     else:
+#         subtable = random.sample(unrelated_table, 1381)    
+#     subtable = subtable + related_table
+#     random.shuffle(subtable)
+#     with open("../table/" + name_list[i], "wb") as fp:
+#         pickle.dump(subtable, fp)
+
+subtable = random.sample(unrelated_table, 6905)
+for i in range(3):
+    subtable += related_table
+
+random.shuffle(subtable)
+with open("../table/table100.txt",  "wb") as fp:
+    pickle.dump(subtable, fp)
